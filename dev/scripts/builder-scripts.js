2fed0d1de80face281415d84eee353c15f23ec51
medonosec <medonosec@gmail.com>
2015-08-06 18:20:50 +0300

    add some modifications to menu

--- dev/scripts/builder-scripts.js
+++ dev/scripts/builder-scripts.js
@@ -1,6 +1,7 @@
 (function ($) {
   /*nav*/
   var navWrap = $("#nav_wrap");
+  $( "#nav_wrap" ).hide();
 
   $("#navTab a").tab("show");
 
@@ -12,6 +13,25 @@
   $("#nav_wrap").find(".template_close").click(function () {
     navWrap.removeClass("template_open");
   });
+
+  var obj = document.querySelectorAll('.nav-icon');
+  for(var i = obj.length -1;i>=0;i--){
+      var toggle = obj[i];
+      toggleSwitch(toggle);
+  }
+  
+  function toggleSwitch(toggle) {
+    toggle.addEventListener("click",function() {
+      if(this.classList.contains("active") === true) {
+        this.classList.remove("active");
+        $( "#nav_wrap" ).hide();
+      }
+      else {
+        this.classList.add("active");
+        $( "#nav_wrap" ).show();
+      }
+    });
+  }
   /*end of nav*/
 
 })(jQuery);
