ecb016326b3b7c816ae251bd0f17627297d7ccfb
medonosec <medonosec@gmail.com>
2015-08-17 09:55:19 +0300

    Change the download page 'function'

--- src/scripts/builder-scripts.js
+++ src/scripts/builder-scripts.js
@@ -33,31 +33,6 @@
   });
   /*end of nav*/
 
-  /*download*/
-  $("#download_page").on("click", function () {
-    var $editButVideo = $(".video-over").detach();
-    var $editButMap = $(".map-over").detach();
-    var zip = new JSZip();
-    var bootstrapCssSrc = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">';
-    var jquerySrc = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">';
-    var bootstrapJsSrc = '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>';
-    var title = $("#page_title").val();
-    var header = '<!DOCTYPE html><html><head><title>' + title + '</title><meta charset="utf-8">' + bootstrapCssSrc + '<link rel="stylesheet" href="css/style.css">'+ model.newFontLinkTag +'</head><body>';
-    var body = $("#build_wrap").html();
-    var footer = jquerySrc + bootstrapJsSrc + '</body></html>';
-    var styles = model.styleInRow.replace( /[\s\n\r]+/g, ' ');
-    zip.file("public/index.html", header + body + header);
-    zip.file("public/css/style.css", styles);
-
-    var content = zip.generate({
-      type: "blob"
-    });
-    saveAs(content, title + ".zip");
-    $(".block-video").find("iframe").parent().append($editButVideo);
-    $(".block-map").find("iframe").parent().append($editButMap);
-  });
-  /*end of download*/
-
   /*delete page*/
   $("#delete_page").on("click", function () {
     $("#build_wrap").empty();
@@ -74,32 +49,43 @@
     tmplsFooterInMenuView.render();
   });
   /*end of delete*/
-
-  $("#clear").on("click", function () {
-    $(".modal").removeClass("active");
-    $(".modal").addClass("fade");
+ 
+  /*Save the page*/
+  $("#download_page").on("click", function () {
+    $("#modal-save").removeClass("active");
+    $("#modal-save").addClass("fade");
   });
 
   $("#save").on("click", function () {
-    $("#download_page").click();
-    model.containerTemplateBlockList = [];
-    model.containerTemplateHeaderList = "";
-    model.containerTemplateFooterList = "";
-    tmplsHeaderInMenuView.render();
-    tmplsBlocksInMenuView.render();
-    tmplsFooterInMenuView.render();
-    controller.getTemplate();
+    var $editButVideo = $(".video-over").detach();
+    var $editButMap = $(".map-over").detach();
+    var zip = new JSZip();
+    var bootstrapCssSrc = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">';
+    var jquerySrc = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">';
+    var bootstrapJsSrc = '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>';
+    var title = $("#page_title").val();
+    var header = '<!DOCTYPE html><html><head><title>' + title +
+                 '</title><meta charset="utf-8">' + bootstrapCssSrc +
+                 '<link rel="stylesheet" href="css/style.css">'+ model.newFontLinkTag +
+                 '</head><body>';
+    var body = $("#build_wrap").html();
+    var footer = jquerySrc + bootstrapJsSrc + '</body></html>';
+    var styles = model.styleInRow.replace( /[\s\n\r]+/g, ' ');
+    zip.file("public/index.html", header + body + header);
+    zip.file("public/css/style.css", styles);
+    var content = zip.generate({
+      type: "blob"
+    });
+    saveAs(content, title + ".zip");
+    $(".block-video").find("iframe").parent().append($editButVideo);
+    $(".block-map").find("iframe").parent().append($editButMap);
+    $("#delete_page").click();
   });
 
-  $("#remove").on("click", function () {
-    model.containerTemplateBlockList = [];
-    model.containerTemplateHeaderList = "";
-    model.containerTemplateFooterList = "";
-    tmplsHeaderInMenuView.render();
-    tmplsBlocksInMenuView.render();
-    tmplsFooterInMenuView.render();
-    controller.getTemplate();
+  $("#remove").on("click", function(){
+    $("#delete_page").click();
   });
+  /*end of save*/
 
   $('.template_wrap').perfectScrollbar();
   $('.tmplsBlocksInMenu').perfectScrollbar();
