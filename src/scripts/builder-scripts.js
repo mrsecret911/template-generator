870b36fbaaee3c8c7a7d1c0ec779574cbf0b0533
medonosec <medonosec@gmail.com>
2015-08-19 23:26:54 +0300

    Fixed download page logic and some other bugs

--- src/scripts/builder-scripts.js
+++ src/scripts/builder-scripts.js
@@ -70,24 +70,25 @@
     var zip = new JSZip();
     var bootstrapCssSrc = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">';
     var jquerySrc = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>';
+    var paralax = '<script src="https://rawgit.com/TarickMd/parallaxMd/master/parallaxMd.js"></script>';
     var bootstrapJsSrc = '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>';
     var title = $("#page_title").val();
-    var sctipts = '<script>$(".carousel-control").on("click",function(){var s=$(this),c=$(this).closest(".carousel");return c.carousel(s.hasClass("left")?"prev":"next"),!1}),$(".carousel-indicators").find("li").on("click",function(){var s=$(this),c=s.index();return s.closest(".carousel").carousel(c),!1});</script>';
     var header = '<!DOCTYPE html><html><head><title>' + title +
             '</title><meta charset="utf-8">' + bootstrapCssSrc +
-            '<link rel="stylesheet" href="css/style.css">' + model.newFontLinkTag +
-            '</head><body>';
+            '<link rel="stylesheet" href="css/style.css">' + model.newFontLinkTag + jquerySrc + paralax +
+            '</head>'+ '<body>';
     var body = $("#build_wrap").html();
-    var footer = jquerySrc + bootstrapJsSrc + sctipts + '</body></html>';
+    var footer = bootstrapJsSrc + '</body></html>';
     var styles = model.styleInRow.replace(/[\s\n\r]+/g, ' ');
     var html = header + body + footer;
     var clearedHtml = html.replace(/\scontenteditable="true"|\sdraggable="true"|\sdrag_and_drop|\sdraggable|\sdata-hover=""|\sdata-element="[^"]*"/g, "");
 
     zip.file("public/index.html", html);
     zip.file("public/css/style.css", styles);
-    var content = zip.generate({type:"blob"});
+    var content = zip.generate({
+      type: "blob"
+    });
     saveAs(content, title + ".zip");
-    
     $(".block-video").find("iframe").parent().append($editButVideo);
     $(".block-map").find("iframe").parent().append($editButMap);
   });
