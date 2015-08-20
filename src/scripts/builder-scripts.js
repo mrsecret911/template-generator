f2ba86c5063b0aa5d4aabdd8f25381b26ff6f563
Станіслав <stanislavmelnychek@gmail.com>
2015-08-20 05:32:03 +0300

    fixed bugs in project

--- src/scripts/builder-scripts.js
+++ src/scripts/builder-scripts.js
@@ -64,21 +64,23 @@
     var $editButMap = $(".map-over").detach();
     var zip = new JSZip();
     var bootstrapCssSrc = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">';
+    var fontawesome = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">';
     var jquerySrc = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>';
     var paralax = '<script src="https://rawgit.com/TarickMd/parallaxMd/master/parallaxMd.js"></script>';
     var bootstrapJsSrc = '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>';
     var title = $("#page_title").val();
     var header = '<!DOCTYPE html><html><head><title>' + title +
-            '</title><meta charset="utf-8">' + bootstrapCssSrc +
+            '</title><meta charset="utf-8">' + bootstrapCssSrc + fontawesome +
             '<link rel="stylesheet" href="css/style.css">' + model.newFontLinkTag + jquerySrc + paralax +
             '</head>'+ '<body>';
     var body = $("#build_wrap").html();
+    var scripts = '<script>$(".carousel-control").on("click",function(){var s=$(this),c=$(this).closest(".carousel");return c.carousel(s.hasClass("left")?"prev":"next"),!1}),$(".carousel-indicators").find("li").on("click",function(){var s=$(this),c=s.index();return s.closest(".carousel").carousel(c),!1});</script>';
     var footer = bootstrapJsSrc + '</body></html>';
     var styles = model.styleInRow.replace(/[\s\n\r]+/g, ' ');
-    var html = header + body + footer;
+    var html = header + body + scripts  + footer;
     var clearedHtml = html.replace(/\scontenteditable="true"|\sdraggable="true"|\sdrag_and_drop|\sdraggable|\sdata-hover=""|\sdata-element="[^"]*"/g, "");
 
-    zip.file("public/index.html", html);
+    zip.file("public/index.html", clearedHtml);
     zip.file("public/css/style.css", styles);
     var content = zip.generate({
       type: "blob"
@@ -86,6 +88,7 @@
     saveAs(content, title + ".zip");
     $(".block-video").find("iframe").parent().append($editButVideo);
     $(".block-map").find("iframe").parent().append($editButMap);
+    return false;
   });
   /*end of save*/
 
